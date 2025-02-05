9b8b9694c2698f08544487f28a834b275d3b0f4b
marina04-dev <marinachatzeephraimidou@gmail.com>
2025-02-05 12:14:58 -0800

    Create Exercise 9: Check Palindrome Number.py

---
+++ Exercise 9: Check Palindrome Number.py
@@ -0,0 +1,21 @@
+"""Exercise 9: Check Palindrome Number"""
+"""Write a Python code to check if the given number is palindrome. A palindrome number is a number that is the same after reverse. For example, 545 is the palindrome number."""
+def palindrome(number):
+    print("original number", number)
+    original_num = number
+    
+    # reverse the given number
+    reverse_num = 0
+    while number > 0:
+        reminder = number % 10
+        reverse_num = (reverse_num * 10) + reminder
+        number = number // 10
+
+    # check numbers
+    if original_num == reverse_num:
+        print("Given number palindrome")
+    else:
+        print("Given number is not palindrome")
+
+palindrome(121)
+palindrome(125)
