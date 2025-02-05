73ca93321cf85f2f85130869573296ba7e89b2cf
marina04-dev <marinachatzeephraimidou@gmail.com>
2025-02-05 08:33:02 -0800

    Create Exercise 4: Remove first n characters from a string.py

---
+++ Exercise 4: Remove first n characters from a string.py
@@ -0,0 +1,10 @@
+"""Exercise 4: Remove first n characters from a string"""
+"""Write a Python code to remove characters from a string from 0 to n and return a new string."""
+def remove_chars(word, n):
+    print('Original string:', word)
+    x = word[n:]
+    return x
+
+print("Removing characters from a string")
+print(remove_chars("pynative", 4))
+print(remove_chars("pynative", 2))
